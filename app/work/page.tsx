4894c2b39cf6cbe740ca851a7fc39b641fb6e5fe
CyDreal <ditodiaz25@gmail.com>
2025-01-23 23:39:58 +0700

    latihan routing

---
+++ app/work/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Wokr = () => {
+  return (
+    <div>Wokr page</div>
+  )
+}
+
+export default Wokr
---
+++ tailwind.config.ts
@@ -0,0 +1,41 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+import type { Config } from "tailwindcss";
+
+export default {
+    darkMode: ["class"],
+    content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+	container: {
+		center: true,
+		padding: "15px",
+	},
+	screens: {
+		sm: '640px',
+		md: '768px',
+		lg: '960px',
+		xl: '1280px',
+	},
+	fontFamily: {
+		primary: 'vart(--font-jetbrainsMono)',
+	},
+	extend: {
+		colors: {
+			primary: "#1c1c22",
+			accent: {
+				DEFAULT: "#00ff99",
+				hover: "#00e187",
+			}
+		},
+		borderRadius: {
+			lg: 'var(--radius)',
+			md: 'calc(var(--radius) - 2px)',
+			sm: 'calc(var(--radius) - 4px)'
+		}
+	}
+	},
+	plugins: [require("tailwindcss-animate")],
+} satisfies Config;
